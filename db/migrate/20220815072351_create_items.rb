260b0baea4fc126767ad3d3d23a4fddadee44d63
EC2 Default User <ec2-user@ip-172-31-19-196.us-west-1.compute.internal>
2022-08-15 08:29:06 +0000

    model追記

--- db/migrate/20220815072351_create_items.rb
+++ db/migrate/20220815072351_create_items.rb
@@ -2,6 +2,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
       t.integer :genre_id
+      t.string :item_name
       t.timestamps
     end
   end
